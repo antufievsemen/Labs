506400fd7bd692b260ec7597575104bb0d0f852e
Alex Antufev <12766383+AlexAntufiev@users.noreply.github.com>
2019-11-01 16:50:35 +0300

    Update MyProperties.java

--- Labs/src/task5/MyProperties/MyProperties.java
+++ Labs/src/task5/MyProperties/MyProperties.java
@@ -4,26 +4,27 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.*; нельзя так делать, все такие импорты разворачивать
 
 
-public class MyProperties{
+public class MyProperties{ Properties
 
-  private Map<String, String> hashMap = new HashMap<>();
+  private Map<String, String> hashMap = new HashMap<>(); инициализация в конструкторе + ты знаешь размер мапы
 
-  public MyProperties(String pathToFile) throws IOException {
-    List<String> list = Files.readAllLines(Paths.get(pathToFile), StandardCharsets.UTF_8);
+  public MyProperties(String pathToFile) throws IOException { лови IOException, бросай новое исключение и напиши понятное сообщение пользователю
+    List<String> list = Files.readAllLines(Paths.get(pathToFile), StandardCharsets.UTF_8); Если файл без расширения .properties?
     for(String line: list){
-      String[] arrayOfKeyValue = line.split("=");
-      hashMap.put(arrayOfKeyValue[0], arrayOfKeyValue[1]);
+      String[] arrayOfKeyValue = line.split("="); там может не быть =
+      hashMap.put(arrayOfKeyValue[0], arrayOfKeyValue[1]); слева от = могут быть пусто, надо ругаться и говорить, что неверный формат, а вот значение может быть пустое
+      не должно быть два одинаковых ключа! но мапа позволит тебе вставить
     }
   }
 
-  public String getValue(String key){
+  public String getValue(String key){ getPropertyByName
     return hashMap.get(key);
   }
 
-  public Collection<String> getValues(){
+  public Collection<String> getValues(){ getProperties
     return hashMap.values();
   }
 
