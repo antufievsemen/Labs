e15e7306178bb4512a6c85b5791c5a43846f5bc9
Semen <semenantufiev@gmail.com>
2019-11-05 22:28:01 +0300

    Rework main and propetries-file

--- Labs/src/task5/Main.java
+++ Labs/src/task5/Main.java
@@ -1,14 +1,10 @@
-import MyProperties.MyProperties;
+import MyProperties.Properties;
 
 import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
-    try {
-      MyProperties myProperties = new MyProperties("C:\\Users\\antuf\\Projects\\Lab5\\src\\MyProperties\\FileWithPasswords");
-      System.out.println(myProperties.toString());
-    } catch (IOException e) {
-      System.err.println("File can`t read");
-    }
+    Properties myProperties = new Properties("C:\\Users\\antuf\\Projects\\Lab5\\src\\MyProperties\\FileWithPasswords.properties");
+    System.out.println(myProperties.toString());
   }
 }
