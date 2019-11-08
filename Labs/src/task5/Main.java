2d1609c563cc19d5e8a5e37d17ec92b9789f0a03
Semen <semenantufiev@gmail.com>
2019-11-08 23:13:22 +0300

    Rewrite path to the properties file
    Rewrite absolute path to relative path

--- Labs/src/task5/Main.java
+++ Labs/src/task5/Main.java
@@ -1,9 +1,10 @@
 import MyProperties.Properties;
 
+import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
-    Properties myProperties = new Properties("C:\\Users\\antuf\\Projects\\Lab5\\src\\MyProperties\\FileWithPasswords.properties");
+    Properties myProperties = new Properties("src\\MyProperties\\FileWithPasswords.properties");
     System.out.println(myProperties.toString());
   }
 }
